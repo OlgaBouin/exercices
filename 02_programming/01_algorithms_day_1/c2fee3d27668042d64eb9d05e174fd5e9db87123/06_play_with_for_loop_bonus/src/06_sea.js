e044e7168630292dbb17639528cada1954a1ce33
OlgaBouin <olga.bouin@adeo.com>
2021-03-16 10:37:47 +0100

    plus exo6 D1

--- 02_programming/01_algorithms_day_1/c2fee3d27668042d64eb9d05e174fd5e9db87123/06_play_with_for_loop_bonus/src/06_sea.js
+++ 02_programming/01_algorithms_day_1/c2fee3d27668042d64eb9d05e174fd5e9db87123/06_play_with_for_loop_bonus/src/06_sea.js
@@ -13,52 +13,71 @@
 // ~~~~~~X~~~~~~~~~~~~~~~~~~~~~~~
 // ```
 
-//methode 1
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~X~~~~~");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~0~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~~~~~~~~~~~~~0~~~~~~~~~~~~");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~~X~~~~~~~~~~~~~~~~~~~~~~~");
 
 /*
-function createThirtyWavesLine(){
-  let sousArray=[];
-  for(let j=0;j<30;j++){
+//methode 1
+
+function createThirtyWavesLine() {
+  let sousArray = [];
+  for (let j = 0; j < 30; j++) {
     sousArray.push("~");
   }
   return sousArray;
 }
 
-function createSeaNineLinesThirtyColumns(){
-  let doubleArray=[];
-  for(let j=0;j<9;j++){
+function createSeaNineLinesThirtyColumns() {
+  let doubleArray = [];
+  for (let j = 0; j < 9; j++) {
     doubleArray.push(createThirtyWavesLine());
   }
-  doubleArray[1][24]="X";
-  doubleArray[8][6]="X";
-  doubleArray[3][5]="0";
-  doubleArray[6][17]="0";
+  doubleArray[1][24] = "X";
+  doubleArray[8][6] = "X";
+  doubleArray[3][5] = "0";
+  doubleArray[6][17] = "0";
   return doubleArray;
 }
 
-function displayDoubleArray(doubleArray){
-  let stringToDisplay="";
-  for(let i=0;i<doubleArray.length;i++){
-    stringToDisplay=stringToDisplay+doubleArray[i].join("")+"\n";
+function displayDoubleArray(doubleArray) {
+  let stringToDisplay = "";
+  for (let i = 0; i < doubleArray.length; i++) {
+    stringToDisplay = stringToDisplay + doubleArray[i].join("") + "\n";
   }
-  return stringToDisplay.slice(0,-1);
+  return stringToDisplay.slice(0, -1);
 }
-console.log(displayDoubleArray(createSeaNineLinesThirtyColumns()));
-*/
+
+function fn() {
+  console.log(displayDoubleArray(createSeaNineLinesThirtyColumns()));
+}
+
+fn();
 
 //methode 2
 
-for(let i=1;i<10;i++){
-  for(let j=1;j<31;j++){
-    if ((i===2&&j===25)||(i===9&&j===7)){
-      process.stdout.write("X");
+
+function sea() {
+  for (let i = 1; i < 10; i++) {
+    for (let j = 1; j < 31; j++) {
+      if ((i === 2 && j === 25) || (i === 9 && j === 7)) {
+        process.stdout.write("X");
+      } else if ((i === 4 && j === 6) || (i === 7 && j === 18)) {
+        process.stdout.write("0");
+      } else {
+        process.stdout.write("~");
+      }
     }
-    else if ((i===4&&j===6)||(i===7&&j===18)){
-      process.stdout.write("0");
-    }
-    else{
-      process.stdout.write("~");
-    }
+    console.log("");
   }
-  console.log("");
 }
+
+sea();
+*/
